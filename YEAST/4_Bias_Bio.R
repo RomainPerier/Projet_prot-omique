01c237ee2b36f4a487bc09cffaeb4acdeebd84a4
aurianegbt <aurianegabaut@gmail.com>
2023-02-27 10:37:28 +0100

    Updtae

--- YEAST/4_Bias_Bio.R
+++
@@ -1,5 +0,0 @@
-
-# faire BF BH et voir combien de fp sont estimer par V* 
-# tester BH sur les p-val restreintes m= avec les plus petites
-# Sk={pvaleur rejete par BH par les k plus petites valeurs} -> pas de garantis statistiques 
-# mais tester V* qui a des garantis 
