59f0248281ddfac360033e57d37e382265da0bca
aurianegbt <aurianegabaut@gmail.com>
2023-02-27 14:15:15 +0100

    Hybridation on Vstar
    Hybridation with min on zeta is better in some cases

--- 5_Hybrid.R
+++ 5_Hybrid.R
@@ -1,18 +1,108 @@
-#  VDKWM, V HB zetak 
-#     zeta -> dekwm au dessus puis hb sur feuilles 
+library(sanssouci)
+library(cgwtools)
+library(ggplot2)
+load('save/data.RData')
 #  hybride Vdkwm et Vhb 
 # V*DKHM(zeta.dkwm(alpha)), et  V*HB(zeta.HB(alpha))
 # plusieurs manières d'hybdrider 
-# 1. Vhybri = min (VDKWM(S),VHB(S))   -> mais perds garantis stats sauf si avec poids 
-#     i.e. Vhybrid = min (VDKWM(zeta(gamma alpha)),VHB(zeta(1-gammaalpha))) 
-# 2. faire un min avec les zeta ? 
-#   car même R_k ! 
-#     Vhybdri' = V*(min(zeta.DKWM(gamma alpha),zeta.BH(1- gamma alpha))) tjrs inférieur à la premièreb manièer -> garantis stats 
-#    Egalité stricte ??? -> si oui mieux ! 
-#    boucle for qui parcourt les zeta et 
 #
 #  varier gqamma aussi 
 #
 #  en théorie gamma petit n'empêche pas la perforcmance de v* 
 #
 # papier dbnr gamma=0.2
+
+## Hyper paramètres ------------------------------------------------------------
+
+list_gamma = 0.1*(1:9)
+alpha = 0.05
+
+## Première hybridation --------------------------------------------------------
+# 1. Vhybri = min (VDKWM(S),VHB(S))   -> mais perds garantis stats sauf si avec poids 
+#     i.e. Vhybrid = min (VDKWM(zeta(gamma alpha)),VHB(zeta(1-gammaalpha))) 
+m=nrow(proteom)
+C=final_tree[[2]]
+leaf_list=final_tree[[1]]
+df_hybrid=data.frame()
+
+for (gamma in list_gamma){
+  # Calcul tout les K
+  K = 250
+  max=m%/%K
+  V_hybrid = rep(0,max)
+  TP_V_hybrid = rep(0,max)
+  ZL_1 = zetas.tree(C,leaf_list,zeta.DKWM,pval,gamma*alpha)
+  ZL_2 = zetas.tree(C,leaf_list,zeta.HB,pval,(1-gamma)*alpha)
+  for (i in 1:max){
+    S = line_sorted_by_pval[1:(i*K)]
+    V_hybrid[i] <- min(V.star(S,C,ZL_1,leaf_list),V.star(S,C,ZL_2,leaf_list))
+    TP_V_hybrid[i] <- i*K - V_hybrid[i]
+  }
+  df_aux <- data.frame(Index=(1:max),Gamma=as.character(gamma),V_hybrid=V_hybrid,TP_V_hybrid=TP_V_hybrid)
+  df_hybrid <- rbind(df_hybrid,df_aux)
+}
+
+
+## Deuxième hybridation --------------------------------------------------------
+# 2. faire un min avec les zeta ? 
+#   car même R_k ! 
+#     Vhybdri' = V*(min(zeta.DKWM(gamma alpha),zeta.BH(1- gamma alpha))) tjrs inférieur à la premièreb manièer -> garantis stats 
+#    Egalité stricte ??? -> si oui mieux ! 
+#    boucle for qui parcourt les zeta et 
+
+m=nrow(proteom)
+C=final_tree[[2]]
+leaf_list=final_tree[[1]]
+df_hybrid2=data.frame()
+
+for (gamma in list_gamma){
+  df_aux=data.frame()
+  # Calcul tout les K
+  K = 250
+  max=m%/%K
+  V_hybrid = rep(0,max)
+  TP_V_hybrid = rep(0,max)
+  ZL_1 = zetas.tree(C,leaf_list,zeta.DKWM,pval,gamma*alpha)
+  ZL_2 = zetas.tree(C,leaf_list,zeta.HB,pval,(1-gamma)*alpha)
+  ZL=list()
+  H = length(C)
+  for (h in 1:H){
+    ZL[[h]] <- apply(data.frame(ZL_1[[h]],ZL_2[[h]]),1,FUN=min)
+  }
+  for (i in 1:max){
+    S = line_sorted_by_pval[1:(i*K)]
+    auxV=V.star(S,C,ZL,leaf_list)
+    TP_V_hybrid[i] <- i*K - auxV
+    V_hybrid[i] <- auxV
+  }
+  df_aux <- data.frame(Index=(1:max),Gamma=as.character(gamma),V_hybrid=V_hybrid,TP_V_hybrid=TP_V_hybrid)
+  df_hybrid2 <- rbind(df_hybrid2,df_aux)
+}
+
+
+## Plot ------------------------------------------------------------------------
+
+ggplot(df_hybrid,aes(x=Index,y=TP_V_hybrid,color=Gamma))+
+  geom_line(lwd=1) +  
+  ylim(c(0,200))+
+  ggtitle('Hybrid - Yeast Data')
+
+ggplot(df_hybrid,aes(x=Index,y=TP_V_hybrid,color=Gamma))+
+  geom_line(lwd=1) +  
+  ylim(c(0,200))+
+  ggtitle('Hybrid - Yeast Data')
+
+df <- df_hybrid
+df[,'TP_V_hybrid'] <- df_hybrid2[,'TP_V_hybrid'] - df_hybrid[,'TP_V_hybrid']
+
+ggplot(df,aes(x=Index,y=TP_V_hybrid,color=Gamma))+
+  geom_line(lwd=1) +  
+  ylim(c(-2,2))+
+  ggtitle('Diff Hybrid - Yeast Data')
+
+# la 2e mieux dans certain cas 
+
+
+
+
+
