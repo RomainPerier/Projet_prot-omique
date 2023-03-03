ae551240c998eb2b6f6dcc760c606d7c64e03ca8
aurianegbt <aurianegabaut@gmail.com>
2023-03-03 15:54:01 +0100

    Update 3.3_JER_control.R
    Test without violated region

--- 3.3_JER_control.R
+++ 3.3_JER_control.R
@@ -1,14 +1,14 @@
 library(sanssouci)
 library(ggplot2)
 library(cgwtools)
+library(stringr)
+library(reshape2)
 library(doBy)
 load('save/final_tree.RData')
+load('save/proteom.RData')
 load('save/pval.RData')
-# JER : voir dans quel Rk JER violé dans notre régionnement 
-# => trouver quelles zeta_k foire le JER !
-# voir ce qu'il y a a conservé, qu'est ce qui fonctionne fonctionne pas 
-# on va regarder quel Rk viole le JER 
-# zeta contrôle le JER sur RK ie IP(FP dans Rk <= zeta_k)>= 1-alpha 
+load('save/split_data.RData')
+load('save/res_ordered.RData')
 
 ## Contrôle du JER sur les feuilles  -------------------------------------------
 alpha = 0.05
@@ -44,4 +44,106 @@ prot_violated=split_data[line]
 len_prop_violated=unlist(lapply(splitBy(formula = ~ Leading_razor_protein,proteom[Rk_violated,]),function(x){dim(x[1])}))
 len_prot=unlist(lapply(prot_violated,function(x){dim(x[1])}))
 
-ZL_node=ZL_DKWM[[2]]
+## Suppression des peptides problèmatiques -------------------------------------
+
+proteom_jer_proof = proteom[-Rk_violated,]
+
+# Test 
+
+  res=read.csv("Fichier/proteom_jer_proof_test.txt",header=TRUE,sep="\t")[,c('id','Pvalue','Leading_razor_protein')]
+  pval= res$Pvalue
+
+# Plot 
+  
+  plot(ecdf(pval))
+  lines(c(0,1),c(0,1),col='red',lwd=2)  
+
+  res_ordered_jer_proof = res_ordered[-Rk_violated,]
+  res_ordered_jer_proof$Pvalue = pval  
+
+# Structure 
+  
+  split_data_jer_proof=splitBy(formula = ~ Species + Leading_razor_protein,res_ordered_jer_proof)[]
+  
+  
+  m=nrow(res_ordered_jer_proof)
+  n_leaf=length(split_data_jer_proof)
+  
+  
+  taille_leaf=rep(0,n_leaf) 
+  for (i in 1:n_leaf){
+    taille_leaf[i]<-nrow(split_data_jer_proof[[i]])
+  }
+  taille_leaf_cum=cumsum(taille_leaf) 
+  
+  
+  leaf_list=list(1:(taille_leaf_cum[1]))
+  for (i in 2:n_leaf){
+    j=i-1
+    leaf_list=append(leaf_list,list((taille_leaf_cum[j]+1):(taille_leaf_cum[i])))
+  }
+  rm(taille_leaf,taille_leaf_cum,i,j)
+  aux=!str_detect(names(split_data_jer_proof),"ups")
+  n_levure=length(aux[aux])    
+  n_ups=length(aux[!aux])    
+  n_species_cum=c(n_levure,n_levure+n_ups)
+  rm(aux) 
+  
+  C3=list(c(1,1))   
+  for (i in 2:n_leaf){
+    C3<-append(C3,list(c(i,i)))
+  }
+  C=list(list(c(1,n_leaf)),list(c(1,n_levure),c(n_levure+1,n_leaf)),C3)    
+  rm(C3)
+  
+  final_tree_jer_proof=list(leaf_list,C)
+  
+## Bornes ---------------------------------------------------------------------- 
+C=final_tree_jer_proof[[2]]
+leaf_list=final_tree_jer_proof[[1]]
+m=nrow(proteom_jer_proof)
+
+# TP 
+TP=cumsum(res_ordered_jer_proof[order(pval),]$Species=='ups')
+df_bornes = data.frame(Index=1:m)
+
+df_bornes <- cbind(df_bornes,TP)
+
+alpha=0.05
+K = 200 # Calculer tout les K lignes
+ZL_DKWM=zetas.tree(C,leaf_list,zeta.DKWM,pval,alpha)
+ZL_HB = zetas.tree(C,leaf_list,zeta.HB,pval,alpha)
+#ZL_refined=zetas.tree.refined(C,leaf_list,zeta.DKWM,pval,alpha)
+
+Vstar_DKWM=rep(0,m%/%K)
+Vstar_HB=rep(0,m%/%K)
+#Vstar_refined=rep(0,m%/%K)
+line_sorted_by_pval_jer_proof = order(pval)
+for (i in 1:(m%/%K)){
+  S=line_sorted_by_pval_jer_proof[1:(K*i)]
+  Vstar_DKWM[i]<-V.star(S,C,ZL_DKWM,leaf_list)
+  Vstar_HB[i]<-V.star(S,C,ZL_HB,leaf_list)
+  #Vstar_refined[i] <- V.star(S,C,ZL_refined,leaf_list)
+  print(i)
+}
+# Vsimes : 
+thr=alpha/m*(1:m)
+Vsimes=sanssouci:::curveMaxFP(pval,thr)[1:(m%/%K)*K]
+
+df <- cbind(df_bornes[1:(m%/%K)*K,],Vstar_DKWM,Vstar_HB,Vsimes)
+
+#Plot : 
+df_plot =  cbind(df[,1:2],TP_Vsimes=df[,'Index']-df[,'Vsimes']
+                 ,TP_Vstar_DKWM=df[,'Index']-df[,'Vstar_DKWM']
+                 ,TP_Vstar_HB=df[,'Index']-df[,'Vstar_HB'])
+                #,TP_Vstar_refined=df[,'Index']-df[,'Vstar_refined'])
+
+df_plot = melt(df_plot, id.vars = "Index")
+
+
+ggplot(df_plot,aes(x=Index,y=value,color=variable))+
+  geom_line(lwd=1) +  
+  ggtitle('Lower Bound on True Positive in Yeast Data')  
+  
+# still bad 
+  
