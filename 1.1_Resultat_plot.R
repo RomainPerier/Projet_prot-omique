bfc71c7dd1e30d5e70b10502091335faae3e79d1
aurianegbt <aurianegabaut@gmail.com>
2023-03-16 10:48:40 +0100

    test results plot

---
+++ 1.1_Resultat_plot.R
@@ -0,0 +1,21 @@
+library(stringr)
+library(ggplot2)
+load('save/pval.RData')
+load('save/res_ordered.RData')
+source("00_Theme.R")
+theme_set(theme_ben())
+
+## Data_frame ------------------------------------------------------
+m=nrow(res_ordered)
+
+vct_cond=res_ordered$Species
+vct_cond[vct_cond=='levure']<-'H0'
+vct_cond[vct_cond=='ups']<-'H1'
+
+df=data.frame(Pvalue=res_ordered$Pvalue,Condition=vct_cond)
+df <- df[line_sorted_by_pval,]
+
+ggplot(df,aes(x=Pvalue,color=Condition))+
+  stat_ecdf(geom='step',lwd=1)+
+  ggtitle('Réparition des p-valeurs selon la condition')
+
