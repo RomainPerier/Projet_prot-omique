4b2e49a37772710d0248530fe50ded9f3a0b432f
Romain Perier <romain.perier@ens-rennes.fr>
2023-06-09 11:52:01 +0200

    Prétraitement en enlevant toutes les lignes avec une donnée manquante

--- 0_Prétraitement.R
+++ 0_Prétraitement.R
@@ -3,130 +3,39 @@
 
 bdd=read.csv("Fichier/peptides_YEASTUPS.txt",header=TRUE,sep="",blank.lines.skip=TRUE)
 
+
 ## Filtration -------------------------------------------------------
 
+
 ## code pour récupérer les infos sur les colonnes de manière auto
-vectcond=sapply(colnames(bdd), function(i) strsplit(i,"[_]")[[1]][3])
-vectcond=="0.5fmol"
-vectcond=="10fmol"
-# etc... 
+vectcond=lapply(colnames(bdd), function(i) strsplit(i,"[_]")[[1]][3])
+vectcond[is.na(vectcond)]<-FALSE
+set1 <- vectcond=="0.5fmol"
+set2 <- vectcond=="1fmol"
+set3 <- vectcond=="2.5fmol"
+set4 <- vectcond=="5fmol"
+set5 <- vectcond=="10fmol"
+set6 <- vectcond=="25fmol"
+
 
 
 #On retire des données toutes les lignes possédant 
 #un échantillon complet manquant :
 
-Count_Intensity_Sets=cbind(rowSums(bdd[,(3:5)]>0),     # On compte le nombre 
-                           rowSums(bdd[,(6:8)]>0),     # de valeurs mesurées 
-                           rowSums(bdd[,(9:11)]>0),    # pour chaque set :
-                           rowSums(bdd[,(12:14)]>0),   # S'il manque un sets
-                           rowSums(bdd[,(15:17)]>0),   # complet, une de ces
-                           rowSums(bdd[,(18:20)]>0))   # valeurs est à 0.
+Count_Intensity_Sets=cbind(rowSums(bdd[,set1]>0),     # On compte le nombre 
+                           rowSums(bdd[,set2]>0),     # de valeurs mesurées 
+                           rowSums(bdd[,set3]>0),    # pour chaque set :
+                           rowSums(bdd[,set4]>0),   # S'il manque un sets
+                           rowSums(bdd[,set5]>0),   # complet, une de ces
+                           rowSums(bdd[,set6]>0))   # valeurs est à 0.
 
 Count_Intensity=apply(Count_Intensity_Sets,1,min)
 
-bdd=bdd[Count_Intensity!=0,]
+bdd=bdd[Count_Intensity==3,]
 
 rm(Count_Intensity,Count_Intensity_Sets)
 
-## Imputation -------------------------------------------------------
-#Pour chaque set d'échantillons, on remplace la valeur manquante par la
-#moyenne de celles mesurées.
-
-###################################################
-####                     Sets 1                 ### 
-###################################################
-i=3
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-###################################################
-####                     Sets 2                 ### 
-###################################################
-i=6
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-###################################################
-####                     Sets 3                 ### 
-###################################################
-i=9
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-###################################################
-####                     Sets 5                 ### 
-###################################################
-i=12
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-###################################################
-####                     Sets 6                 ### 
-###################################################
-
-i=15
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-###################################################
-####                     Sets 7                 ### 
-###################################################
-
-i=18
-j=i+2
-
-bddaux=bdd[,(i:j)]
-bddaux[bddaux==0]<-NA
-
-ind=which(is.na(bddaux),arr.ind=TRUE)
-bddaux[ind] <- rowMeans(bddaux, na.rm=TRUE)[ind[,1]]
-
-bdd[,(i:j)]<-bddaux
-rm(bddaux,i,j,ind)
-
-#############################
-## Prétraitement terminé ---------------------------------------------
-
+## Pas d'imputation
 
 proteom=bdd
 rm(bdd)
@@ -137,5 +46,4 @@ write.table(proteom, "Fichier/proteom_treated.txt",row.names=FALSE,quote=FALSE,s
 row.names(proteom) <- 1:nrow(proteom)
 save(proteom,file='save/proteom.RData')
 
-
-##ça push ou pas?----------
+rm(list = ls())
